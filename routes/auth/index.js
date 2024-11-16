62329d90a98ffb11f78f1bf844092f673827c9ec
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-11-16 23:33:56 +0530

    Update index.js

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -326,6 +326,12 @@ router.get('/callback', async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: '7d' }
     );
+    res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'Strict',
+    maxAge: 7 * 24 * 60 * 60 * 1000, // (7 days)
+  });
     res.status(200).json({ jwtToken: token });
   } catch (error) {
     console.error('Error during callback processing:', error.message);
