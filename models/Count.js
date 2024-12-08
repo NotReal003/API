bd3b2c1971733beb821f625e992c4c6607561d0e
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-12-08 14:23:48 +0000

    Update

--- models/Count.js
+++ models/Count.js
@@ -1,13 +1,35 @@
 const mongoose = require('mongoose');
 
-const CountSchema = new mongoose.Schema({
-  pageType: { type: String, required: true, unique: true },
-  visits: { type: Number, default: 0 },
-  uniqueVisitors: { type: Number, default: 0 },
-  deviceStats: { type: Map, of: Number, default: {} },
-  browserStats: { type: Map, of: Number, default: {} },
-  referrerStats: { type: Map, of: Number, default: {} },
-  dailyVisits: { type: Map, of: Number, default: {} },
+const countSchema = new mongoose.Schema({
+  pageType: {
+    type: String,
+    required: true,
+  },
+  visits: {
+    type: Number,
+    default: 0,
+  },
+  referrerStats: {
+    type: Map,
+    of: Number,
+  },
+  dailyVisits: {
+    type: Map,
+    of: Number,
+  },
+  weeklyVisits: {
+    type: Map,
+    of: Number,
+  },
+  monthlyVisits: {
+    type: Map,
+    of: Number,
+  },
+  visitTimestamps: {
+    type: [Date],
+  },
 });
 
-module.exports = mongoose.model('Count', CountSchema);
+const Count = mongoose.model('Count', countSchema);
+
+module.exports = Count;
