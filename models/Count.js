d6b62661cc7124af0872af41902303429848db28
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-12-08 20:42:51 +0530

    Update Count.js

--- models/Count.js
+++ models/Count.js
@@ -1,32 +1,26 @@
 const mongoose = require('mongoose');
 
 const countSchema = new mongoose.Schema({
-  pageType: {
-    type: String,
-    required: true,
-  },
-  visits: {
-    type: Number,
-    default: 0,
-  },
-  referrerStats: {
-    type: Map,
-    of: Number,
-  },
-  dailyVisits: {
-    type: Map,
-    of: Number,
-  },
-  weeklyVisits: {
-    type: Map,
-    of: Number,
-  },
-  monthlyVisits: {
-    type: Map,
-    of: Number,
-  },
-  visitTimestamps: {
-    type: [Date],
+  pageType: { type: String, required: true },
+  visits: { type: Number, default: 0 },
+  referrerStats: { type: Map, of: Number },
+  dailyVisits: { type: Map, of: Number },
+  weeklyVisits: { type: Map, of: Number },
+  monthlyVisits: { type: Map, of: Number },
+  visitTimestamps: { type: [Date], default: [] },
+});
+
+// Remove Mongoose metadata when converting to JSON or Object
+countSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    delete ret.__v;
+    delete ret.$__;
+    for (const key in ret) {
+      if (key.startsWith('$')) {
+        delete ret[key];
+      }
+    }
+    return ret;
   },
 });
 
