8be310a65d0f4308eab3e4568ec1ece59ba6b17e
NotReal <117504040+NotReal003@users.noreply.github.com>
2025-04-06 13:36:36 +0530

    Update Request.js

--- models/Request.js
+++ models/Request.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const RequestSchema = new mongoose.Schema({
   id: { type: String, required: true },
   reviewed: { type: String, enum: ['true', 'false'], default: 'false' },
+  escalated: { type: Boolean, default: false },
   username: { type: String, required: false },
   type: { type: String, required: true },
   messageLink: { type: String, required: true },
   reviewMessage: { type: String, default: '' },
   additionalInfo: { type: String },
-  status: { type: String, enum: ['APPROVED', 'DENIED', 'PENDING', 'CANCELLED', 'RESOLVED'], default: 'PENDING' },
+  status: { type: String, enum: ['APPROVED', 'DENIED', 'PENDING', 'CANCELLED', 'RESOLVED', 'ESCALATED'], default: 'PENDING' },
   typeName: { type: String },
   inGameName: { type: String },
 }, { timestamps: true });
