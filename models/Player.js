cd97c3abb4243fecdc25f0fd6e99997a44877e7b
NotReal <117504040+NotReal003@users.noreply.github.com>
2025-04-18 13:59:45 +0530

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const playerSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  xuid: { type: String, required: true },
+  name: { type: String, required: true },
+  xuid: { type: String, unique: true, required: true },
   avatar: { type: String }, // Add avatar field
   searchCount: { type: Number, default: 1 },
 }, { timestamps: true });
